78bdfac7278dc1fd21a0402c91ce5ca2fda38dc1
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-27 13:28:35 -0400

    initial commit

--- src/checker.java
+++ src/checker.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class checker {
+public class checker {
+    public static void main(String[] args) {
+        System.out.println(hasTeen(9, 99, 19));
+        System.out.println(hasTeen(23, 15, 42));
+        System.out.println(hasTeen(22, 23, 34));
+    }
+
+    public static boolean hasTeen(int num1, int num2, int num3) {
+        //One big if/else if/else statement checking each number
+        if (num1 >= 13 && num1 <= 19) {
+            return true;
+        }
+        if (num2 >= 13 && num2 <= 19) {
+            return true;
+        }
+        if (num3 >= 13 && num3 <= 19) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
